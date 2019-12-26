083bc8eb6c647263ede2099ac6843671f8a25dd2
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2019-12-26 08:07:16 -0800

    Secondclass is newly added.

---
+++ corejavaproject/src/com/icicibank/loans/carloans/Secondclass.java
@@ -0,0 +1,25 @@
+package com.icicibank.loans.carloans;
+
+public class Secondclass {
+	int a,b,mul=0;
+	public void mul()
+	{
+		a=5;
+		b=6;
+		mul=a*b;
+		System.out.println("multiplication of two numbers is"+mul);
+	}
+	
+
+	public static void main(String[] args) {
+		System.out.println("hi this is my second class");
+		Secondclass s=new Secondclass();
+		s.mul();
+		
+	
+	
+		
+
+	}
+
+}
