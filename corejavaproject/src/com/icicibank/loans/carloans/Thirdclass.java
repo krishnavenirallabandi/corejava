d15428c1969575068a7a60cf7906317c31043811
krishnavenirallabandi <rallabandi.krishnaveni28@gmail.com>
2019-12-27 11:04:44 -0800

    added new class Fourclass.added new printout messege in thirdclass.

--- corejavaproject/src/com/icicibank/loans/carloans/Thirdclass.java
+++ corejavaproject/src/com/icicibank/loans/carloans/Thirdclass.java
@@ -3,7 +3,8 @@ package com.icicibank.loans.carloans;
 public class Thirdclass {
 
 	public static void main(String[] args) {
-		System.out.println("This is my firstline in thirdclass ");
+		System.out.println("This is my firstline in thirdclass in california branch ");
+		System.out.println("This is secondline in thirdclass in california branch");
 
 	}
 
